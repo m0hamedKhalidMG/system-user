c1892af011fe10f0b3356173953843dfd51efa27
m0hamedKhalidMG <115906020+m0hamedKhalidMG@users.noreply.github.com>
2024-07-03 04:09:25 +0300

    Update Profile.js

--- models/Profile.js
+++ models/Profile.js
@@ -1,26 +1,31 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const bcrypt = require("bcryptjs");
 
 const DiseaseSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  doctorname: {
+    type: String,
+    required: true,
+  },
+  documents: [
+    {
+      name: {
+        type: String,
+      },
+      url: {
+        type: String,
+      },
     },
-      description: {
-        type: String,
-        required: true,
-      },
-      doctorname: {
-        type: String,
-        required: true,
-      },
-    documents: [
-      {
-        type: String, // or mongoose.Schema.Types.ObjectId if documents are separate entities
-      },
-    ],
-  });
+  ],
+});
 
 const ProfileSchema = new Schema({
   phoneNumber: {
@@ -50,7 +55,7 @@ const ProfileSchema = new Schema({
   },
   role: {
     type: String,
-    default: 'user',
+    default: "user",
   },
   address: {
     type: String,
@@ -64,34 +69,35 @@ const ProfileSchema = new Schema({
   location: {
     type: {
       type: String,
-      enum: ['Point'],
+      enum: ["Point"],
     },
     coordinates: {
       type: [Number],
-    }
-  },  
+    },
+  },
   diseases: [DiseaseSchema],
-
-
 });
-ProfileSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) {
-      return next();
-    }
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  });
-  
-  ProfileSchema.methods.matchPassword = async function (password) {
-    return await bcrypt.compare(password, this.password);
-  };
-  
-// Custom validation to enforce either address or location
-ProfileSchema.path('address').validate(function(value) {
-  const addressFields = this.address && this.n_floor && this.n_house;
-  const locationFields = this.location && this.location.coordinates && this.location.coordinates.length === 2;
-  return addressFields || locationFields;
-}, 'Either a full address (address, n_floor, n_house) or location (coordinates) must be provided.');
+ProfileSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
-module.exports = mongoose.model('Profile', ProfileSchema);
+ProfileSchema.methods.matchPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// Custom validation to enforce either address or location
+ProfileSchema.path("address").validate(function (value) {
+  const addressFields = this.address && this.n_floor && this.n_house;
+  const locationFields =
+    this.location &&
+    this.location.coordinates &&
+    this.location.coordinates.length === 2;
+  return addressFields || locationFields;
+}, "Either a full address (address, n_floor, n_house) or location (coordinates) must be provided.");
+
+module.exports = mongoose.model("Profile", ProfileSchema);
