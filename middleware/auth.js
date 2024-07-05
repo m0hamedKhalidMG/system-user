63c2c3b2cc7d1772675cf9decd04a6e57e982a6b
m0hamedKhalidMG <115906020+m0hamedKhalidMG@users.noreply.github.com>
2024-07-05 16:18:16 +0300

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,8 @@
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require('../config');
+const Profile = require('./../models/Profile');
 
-const auth = (req, res, next) => {
+const auth = async (req, res, next) => {
   const token = req.header('x-auth-token');
   if (!token) {
     return res.status(401).json({ msg: 'No token, authorization denied' });
@@ -10,6 +11,9 @@ const auth = (req, res, next) => {
   try {
     const decoded = jwt.verify(token, JWT_SECRET);
     req.user = decoded.user;
+
+    req.profile = await Profile.findById(req.user.id).select('-password');
+
     next();
   } catch (err) {
     res.status(401).json({ msg: 'Token is not valid' });
